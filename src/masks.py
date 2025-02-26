104a69965667aac475ac7a04b54b9f68dba5aac0
Gosha <bakiyev3005@gmail.com>
2025-02-26 11:33:36 +0300

    10.2

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,17 @@ from typing import Union
 def get_mask_card_number(card_number: Union[int, str]) -> str:
     """ " Эта функция скрывает номер карты"""
     card_number = str(card_number)
-    card_number = card_number.strip()
+    card_number = card_number.replace(" ", "")
+
+    if len(card_number) != 16:
+        return "Некорректный номер карты, пожалуйста введите верный номер карты"
+
+    for i in card_number:
+        if i.isdigit():
+            pass
+        else:
+            return "Некорректный номер карты, пожалуйста введите верный номер карты"
+
     part_1 = card_number[0:4]
     part_2 = card_number[4:6]
     part_3 = card_number[-4::]
@@ -20,3 +30,5 @@ def get_mask_account(card_accaunt: Union[int, str]) -> str:
     mask_card_accaunt = "**" + card_accaunt[-4::]
 
     return mask_card_accaunt
+
+print(get_mask_card_number(1.2))
---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def masks_card_1():
+    return "7000 79** **** 6361"
+
+
+@pytest.fixture
+def masks_card_2():
+    return "1023 45** **** 1112"
